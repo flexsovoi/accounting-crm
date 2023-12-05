2da5dd10bc0db91bd4660bb76bf6b0d6882879f2
Danil Develop <developdbdevelop@gmail.com>
2023-12-05 23:09:16 +0300

    config + add libs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,17 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:react-hooks/recommended',
+		'plugin:import/errors',
+		'prettier'
 	],
+	settings: {
+		'import/resolver': {
+			alias: {
+				extensions: ['.ts', '.tsx'],
+				map: [['@/', './src']]
+			}
+		}
+	},
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
 	plugins: ['react-refresh'],
@@ -14,7 +24,7 @@ module.exports = {
 		'@typescript-eslint/no-unused-vars': 'off',
 		'react-refresh/only-export-components': [
 			'warn',
-			{ allowConstantExport: true },
-		],
-	},
+			{ allowConstantExport: true }
+		]
+	}
 }
